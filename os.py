f30857f8b122a8780aa1e01f9c554504983e28c1
michal162371 <me0533190457@gmail.com>
2025-03-05 13:47:11 +0200

    I add a file that called "os" of my files on my computer

---
+++ os.py
@@ -0,0 +1,23 @@
+import os
+from datetime import datetime
+print(os.stat('ty.txt').st_size)
+# time=os.stat('ty.txt').st_mtime
+# os.rename('ty.txt','ty.txt'+str(time))
+print(os.path.exists('./מוזיקה/בנות'))
+things = os.listdir()
+for th in things:
+   if th.endswith('mp3'):
+     os.rename(th,'sing//' + th)
+
+
+for dirpath, dirnames, filenames in os.walk('.\sing'):
+  if len(dirnames)==0 and len(filenames)==0:
+            os.rmdir(dirpath)
+            # הערה: באם יש תיקיה
+            #  ריקה בתוך תקיה ריקה צריך 
+            # להפעיל את הפונקציה כמה פעמים
+
+ 
+mod_time = os.stat('./sing/lo levad.mp3').st_mtime
+print(datetime.fromtimestamp(mod_time))
+
